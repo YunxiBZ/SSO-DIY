26d07be70974e9b6055810e69fe95ee4b3126cdd
Yunxi Zhang <zhangyunxi2012@gmail.com>
2022-05-31 10:21:24 +0200

    SSO-SERCER: add template pug for login form

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,8 +1,20 @@
 import express from 'express';
+import path from 'path';
 const  app = express();
 
+const __dirname = path.resolve();
+
+app.use(
+    '/public/stylesheets',
+    express.static(__dirname + '/public/stylesheets')
+  );
+app.set("view engine", "pug");
+app.set("views", "./src/views");
+
+
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    // res.send('Hello world');
+    res.render("login");
 })
 
 app.listen(3000);
